7e97003e612a0a6344cd516bc45bad1637fa5654
whonor <honorw@foxmail.com>
2025-07-24 15:13:24 +1200

    Update metrics

--- evaluate.py
+++ evaluate.py
@@ -8,28 +8,51 @@ import torch
 from torchmetrics.classification import AUROC
 from torchmetrics.retrieval import RetrievalNormalizedDCG, RetrievalMRR
 
+
+def dcg_score(y_true, y_score, k=10):
+    order = np.argsort(y_score)[::-1]
+    y_true = np.take(y_true, order[:k])
+    gains = 2 ** y_true - 1
+    discounts = np.log2(np.arange(len(y_true)) + 2)
+    return np.sum(gains / discounts)
+
+
+def ndcg_score(y_true, y_score, k=10):
+    best = dcg_score(y_true, y_true, k)
+    actual = dcg_score(y_true, y_score, k)
+    return actual / best
+
+
+def mrr_score(y_true, y_score):
+    order = np.argsort(y_score)[::-1]
+    y_true = np.take(y_true, order)
+    rr_score = y_true / (np.arange(len(y_true)) + 1)
+    return np.sum(rr_score) / np.sum(y_true)
+
+
 def parse_line(l):
     impid, ranks = l.strip('\n').split()
     ranks = json.loads(ranks)
     return impid, ranks
 
+
 def scoring(truth_f, sub_f):
-    auc_metric = AUROC(task='binary', num_classes=2)
-    mrr_metric = RetrievalMRR()
-    ndcg5_metric = RetrievalNormalizedDCG(top_k=5)
-    ndcg10_metric = RetrievalNormalizedDCG(top_k=10)
+    aucs = []
+    mrrs = []
+    ndcg5s = []
+    ndcg10s = []
 
     line_index = 1
     for lt in truth_f:
         ls = sub_f.readline()
         impid, labels = parse_line(lt)
 
-        # Ignore masked impressions
+        # ignore masked impressions
         if labels == []:
             continue
 
         if ls == '':
-            # Empty line: filled with 0 ranks
+            # empty line: filled with 0 ranks
             sub_impid = impid
             sub_ranks = [1] * len(labels)
         else:
@@ -45,48 +68,31 @@ def scoring(truth_f, sub_f):
                 impid
             ))
 
-        y_true = torch.tensor(labels, dtype=torch.float32)
-        y_score = torch.tensor([1. / rank for rank in sub_ranks], dtype=torch.float32)
-        indexes = torch.arange(len(labels))
+        lt_len = float(len(labels))
 
-        # Update metrics
-        auc_metric.update(y_score, y_true.int())
-        mrr_metric.update(y_score, y_true.int(), indexes)
-        ndcg5_metric.update(y_score, y_true.int(), indexes)
-        ndcg10_metric.update(y_score, y_true.int(), indexes)
+        y_true = np.array(labels, dtype='float32')
+        y_score = []
+        for rank in sub_ranks:
+            score_rslt = 1. / rank
+            if score_rslt < 0 or score_rslt > 1:
+                raise ValueError("Line-{}: score_rslt should be int from 0 to {}".format(
+                    line_index,
+                    lt_len
+                ))
+            y_score.append(score_rslt)
+
+        auc = roc_auc_score(y_true, y_score)
+        mrr = mrr_score(y_true, y_score)
+        ndcg5 = ndcg_score(y_true, y_score, 5)
+        ndcg10 = ndcg_score(y_true, y_score, 10)
+
+        aucs.append(auc)
+        mrrs.append(mrr)
+        ndcg5s.append(ndcg5)
+        ndcg10s.append(ndcg10)
 
         line_index += 1
 
-    # Compute final metrics
-    auc = auc_metric.compute().item()
-    mrr = mrr_metric.compute().item()
-    ndcg5 = ndcg5_metric.compute().item()
-    ndcg10 = ndcg10_metric.compute().item()
-
-    return auc, mrr, ndcg5, ndcg10
+    return np.mean(aucs), np.mean(mrrs), np.mean(ndcg5s), np.mean(ndcg10s)
         
 
-if __name__ == '__main__':
-    input_dir = sys.argv[1]
-    output_dir = sys.argv[2]
-
-    submit_dir = os.path.join(input_dir, 'res') 
-    truth_dir = os.path.join(input_dir, 'ref')
-
-    if not os.path.isdir(submit_dir):
-        print("%s doesn't exist" % submit_dir)
-
-    if os.path.isdir(submit_dir) and os.path.isdir(truth_dir):
-        if not os.path.exists(output_dir):
-            os.makedirs(output_dir)
-
-        output_filename = os.path.join(output_dir, 'scores.txt')              
-        output_file = open(output_filename, 'w')
-
-        truth_file = open(os.path.join(truth_dir, "truth.txt"), 'r')
-        submission_answer_file = open(os.path.join(submit_dir, "prediction.txt"), 'r')
-        
-        auc, mrr, ndcg, ndcg10 = scoring(truth_file, submission_answer_file)
-
-        output_file.write("AUC:{:.4f}\nMRR:{:.4f}\nnDCG@5:{:.4f}\nnDCG@10:{:.4f}".format(auc, mrr, ndcg, ndcg10))
-        output_file.close()
