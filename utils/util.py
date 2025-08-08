22cb3f7116c7d95e8de9b8a52776c0ec21e1ffab
whonor <honorw@foxmail.com>
2025-08-08 22:48:22 +1200

    Optimize codes

--- utils/util.py
+++ utils/util.py
@@ -248,7 +248,17 @@ def compute_scores(config: Config, model: nn.Module, corpus, batch_size: int, mo
                                                           news_category, news_subCategory, news_title_text,
                                                           news_title_mask, news_title_entity, news_content_text,
                                                           news_content_mask, news_content_entity).squeeze(1)  # [batch_size]
-            elif config.model == "ILKPNR":
+            elif config.model == "LKPNR":
+                scores[index: index + batch_size] = model(user_ID, user_category, user_subCategory, user_title_text,
+                                                          user_title_mask, user_title_entity, user_content_text,
+                                                          user_content_mask, user_content_entity, user_history_mask,
+                                                          user_history_graph, user_history_category_mask,
+                                                          user_history_category_indices, \
+                                                          news_category, news_subCategory, news_title_text,
+                                                          news_title_mask, news_title_entity, news_content_text,
+                                                          news_content_mask, news_content_entity, history_index,
+                                                          candidate_news_index).squeeze(dim=1)  # [batch_size]
+
             else:
                 scores[index: index + batch_size] = model(user_ID, user_category, user_subCategory, user_title_text,
                                                           user_title_mask, user_title_entity, user_content_text,
