1e73fbb098402964a7e69669f94a238281992d11
whonor <honorw@foxmail.com>
2025-08-23 15:37:52 +1200

    Fix codes

--- models/modules/LKPNR/userEncoders.py
+++ models/modules/LKPNR/userEncoders.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from models.modules.LKPNR.newsencoder import NewsEncoder
+from models.modules.LKPNR.newsEncoders import NewsEncoder
 from models.modules.layers import MultiHeadAttention, Attention
 
 
