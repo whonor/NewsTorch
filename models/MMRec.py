8cddb2ac1cb60183d080d6e1c921fa8f5cfbf850
whonor <w_honor@163.com>
2026-05-10 22:32:50 +1200

    MMRec model pass

--- models/MMRec.py
+++ models/MMRec.py
@@ -1,8 +1,7 @@
-import os
 import torch
 from config import Config
 import torch.nn as nn
-from models.modules.mmrec.news_encoder import NewsEncoder, BertConfig
+from models.modules.mmrec.news_encoder import NewsEncoder
 from models.modules.mmrec.user_encoder import UserEncoder
 
 
@@ -12,47 +11,7 @@ class MMRec(torch.nn.Module):
         self.config = config
         self.model_name = config.model
         self.batch_size = config.batch_size
-        bert_config = BertConfig(
-            vocab_size_or_config_json_file=self.config.vocab_size,
-            hidden_size=self.config.hidden_size,
-            num_hidden_layers=self.config.num_hidden_layers,
-            num_attention_heads=self.config.num_attention_heads,
-            intermediate_size=self.config.intermediate_size,
-            hidden_act=self.config.hidden_act,
-            hidden_dropout_prob=self.config.hidden_dropout_prob,
-            attention_probs_dropout_prob=self.config.attention_probs_dropout_prob,
-            max_position_embeddings=self.config.max_position_embeddings,
-            type_vocab_size=self.config.type_vocab_size,
-            initializer_range=self.config.initializer_range,
-            v_feature_size=self.config.v_feature_size,
-            v_target_size=self.config.v_target_size,
-            v_hidden_size=self.config.v_hidden_size,
-            v_num_hidden_layers=self.config.v_num_hidden_layers,
-            v_num_attention_heads=self.config.v_num_attention_heads,
-            v_intermediate_size=self.config.v_intermediate_size,
-            bi_hidden_size=self.config.bi_hidden_size,
-            bi_num_attention_heads=self.config.bi_num_attention_heads,
-            v_attention_probs_dropout_prob=self.config.v_attention_probs_dropout_prob,
-            v_hidden_act=self.config.v_hidden_act,
-            v_hidden_dropout_prob=self.config.v_hidden_dropout_prob,
-            v_initializer_range=self.config.v_initializer_range,
-            v_biattention_id=self.config.v_biattention_id,
-            t_biattention_id=self.config.t_biattention_id,
-            predict_feature=self.config.predict_feature,
-            fast_mode=self.config.fast_mode,
-            fixed_v_layer=self.config.fixed_v_layer,
-            fixed_t_layer=self.config.fixed_t_layer,
-            in_batch_pairs=self.config.in_batch_pairs,
-            fusion_method=self.config.fusion_method,
-            with_coattention=self.config.with_coattention,
-            image_embedding_dim=self.config.image_embedding_dim
-        )
-        
-        if hasattr(self.config, 'from_pretrained') and self.config.from_pretrained:
-            print(f"Loading pre-trained BERT weights from: {self.config.from_pretrained}")
-            self.news_encoder = NewsEncoder.from_pretrained(self.config.from_pretrained, config=bert_config)
-        else:
-            self.news_encoder = NewsEncoder(config=bert_config)
+        self.news_encoder = NewsEncoder(config)
         self.user_encoder = UserEncoder()
         self.criterion = torch.nn.CrossEntropyLoss()
         self.use_user_embedding = False
